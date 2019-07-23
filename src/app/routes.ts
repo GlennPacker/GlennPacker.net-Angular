281230f83db27591a57bb751ab248dca619ee730
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-07-23 15:31:20 +0100

    basic navigation

---
+++ src/app/routes.ts
@@ -0,0 +1,33 @@
+import {Routes} from "@angular/router";
+
+export const routes: Routes = [
+  // {
+  //   path: 'clients',
+  //   component: ClientComponent,
+  // },{
+  //   path: 'spa',
+  //   component: SpaComponent,
+  // },{
+  //   path: 'front-end',
+  //   component: FrontEndComponent,
+  // },{
+  //   path: 'back-end',
+  //   component: FrontEndComponent,
+  // },{
+  //   path: 'front-end',
+  //   component: ContactComponent,
+  // },
+  // {
+  //   path: '404',
+  //   component: Error404Component
+  // },
+  // {
+  //   path: '/home',
+  //   component: HomeComponent
+  // }
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full'
+  // }
+];
